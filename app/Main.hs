84abe4c0b3ea5a6d0db1ef08cd51d69c4b702927
Hugo Ludemann <hugo.ludemann@gmail.com>
2024-07-28 07:59:51 +1000

    removed logging line

--- app/Main.hs
+++ app/Main.hs
@@ -15,11 +15,7 @@ main = do
   let pattern = args !! 1
   input_line <- getLine
 
-  -- You can use print statements as follows for debugging, they'll be visible when running tests.
-  putStrLn "Logs from your program will appear here"
-
-  -- Uncomment this block to pass stage 1
- if head args /= "-E"
+  if head args /= "-E"
     then do
       putStrLn "Expected first argument to be '-E'"
       exitFailure
