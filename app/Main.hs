37f0753626dc18323be7cf1281e0d95cfbda062b
Hugo Ludemann <hugo.ludemann@gmail.com>
2024-07-31 02:48:37 +1000

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -1,219 +1,186 @@
 module Main where
 
-import Control.Applicative
-import Data.Char
-import Data.List (tails, singleton)
+
+import Control.Monad (void, join)
+import Control.Monad.Combinators.Expr
+import Control.Monad.Identity (Identity, join)
+import Data.Char (isAlphaNum, isDigit)
+import Data.List (tails, singleton, find)
+import Data.Void
+import Data.Maybe (isJust, listToMaybe)
 import System.Environment
 import System.Exit
+import Text.Megaparsec
+import Text.Megaparsec.Char
 
--- TODO: Remove deriving (Show) when complete.
--- TODO: Use megaparsec.
-
---
-------------------------------------------------------------------------------------------------
--- Parsing. This is a toy version until I can fit megaparsec in.
-
--- A parser is a function which takes a String of input to be parsed,
--- which then succeeds or fails.
--- If it succeeds, it returns the parsed value and the remainder of the string.
-newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
-
--- Take a predicate on a character and return a parser which succeeds if the predicate is true.
-satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = Parser f
-  where
-    f [] = Nothing
-    f (x : xs)
-      | p x = Just (x, xs)
-      | otherwise = Nothing
-
--- A parser which expects to see a given character.
-char :: Char -> Parser Char
-char c = satisfy (== c)
-
--- A parser which accepts any character.
-anyChar :: Parser Char
-anyChar = satisfy (\_ -> True)
-
-first :: (a -> b) -> (a, c) -> (b, c)
-first f (x, y) = (f x, y)
-
-instance Functor Parser where
-  fmap f (Parser g) = Parser (fmap (first f) . g)
-
-instance Applicative Parser where
-  pure a = Parser (\s -> Just (a, s))
-  (<*>) (Parser f) (Parser g) =
-    Parser
-      ( \s -> case f s of
-          Just (h, rest) -> fmap (first h) (g rest)
-          _ -> Nothing
-      )
-
-instance Alternative Parser where
-  empty = Parser (const Nothing)
-  (<|>) (Parser f) (Parser g) = Parser (liftA2 (<|>) f g)
-
-zeroOrOne :: Parser a -> Parser [a]
-zeroOrOne p = (singleton <$> p) <|> pure []
-
-zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = oneOrMore p <|> pure []
-
-oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = (:) <$> p <*> zeroOrMore p
-
-
-
---
-------------------------------------------------------------------------------------------------
---
-
-data Element
-  = Literal Char
+-- gadt refactor?
+data Pattern
+  = Empty
+  | Literal Char
   | Digit
-  | AlphaNumeric
-  | WildCard
-  deriving (Show)
+  | AlphaNum
+  | PosCharClass String
+  | NegCharClass String
+  | StartAnchor
+  | EndAnchor
+  | Repeat Quantifier Pattern
+  | Wildcard
+  | Alternative Pattern Pattern
+  | Seq [Pattern] -- COMBination
+  deriving (Eq, Show)
 
-data CharClass
-  = PosCharClass [Char]
-  | NegCharClass [Char]
-  deriving (Show)
+-- Extension: Add Range Int Int constructor.
+data Quantifier = ZeroOrOne | OneOrMore
+  deriving (Eq, Show)
 
-data Anchor
-  = StartOfLine
-  | EndOfLine
-  deriving (Show)
+-- Parsec type parameters demystified: don't do anything fancy with erorrs, consume a String.
+type Parser = Parsec Void String
 
-data Quantifier
-  = OneOrMore
-  | ZeroOrOne
-  deriving (Show)
-
-data Matchable = E Element 
-               | C CharClass
-               | A Anchor
-               | Q Quantifier
-  deriving (Show)
-
--- 
-------------------------------------------------------------------------------------------------
--- Parsers for regex. Look how compact applicative parsing can be.
-
-nonSpecial :: Parser Char
-nonSpecial = satisfy (\c -> c `notElem` "[]^$+?")
-
-literal :: Parser Matchable
-literal =  (E . Literal) <$> nonSpecial
-
-digit :: Parser Matchable
-digit = (E . \_ -> Digit) <$> (char '\\' *> char 'd')
-
-alphanumeric :: Parser Matchable
-alphanumeric = (E . \_ -> AlphaNumeric) <$> (char '\\' *> char 'w')
-
-posCharClass :: Parser Matchable
-posCharClass =  (C . PosCharClass) <$> (char '[' *> oneOrMore nonSpecial <* char ']')
-
-negCharClass :: Parser Matchable
-negCharClass = (C . NegCharClass) <$> (char '[' *> char '^' *> oneOrMore nonSpecial <* char ']')
-
-startOfLine :: Parser Matchable
-startOfLine = (A . \_ -> StartOfLine) <$> char '^'
-
-endOfLine :: Parser Matchable
-endOfLine = (A . \_ -> EndOfLine) <$> char '$'
-
--- Unfortunate naming conflift with the parsing utilities.
-oneOrMore' :: Parser Matchable
-oneOrMore' = (Q . \_ -> OneOrMore) <$> (char '+')
-
-zeroOrOne' :: Parser Matchable
-zeroOrOne' = (Q . \_ -> ZeroOrOne) <$> (char '?')
-
-wildcard :: Parser Matchable
-wildcard = (E . \_ -> WildCard) <$> (char '.')
-
---
-------------------------------------------------------------------------------------------------
--- Main bit.
-
--- A regex that does not contain anchors.
-internal :: Parser [Matchable]
-internal =  zeroOrMore
-    (  alphanumeric
-        <|> digit
-        <|> posCharClass
-        <|> negCharClass
-        <|> oneOrMore'
-        <|> zeroOrOne'
-        <|> wildcard
-        <|> literal
-    )
+regularChar :: Char -> Bool
+regularChar c = c `notElem` "$^+?[]|()."
 
 
+-- Basic parsers. 
+-- These are the parsers for the basic elements of a regular expression, e.g. a literal
+-- character like "a", the string ""\\d", character classes like "[asd]" or "[^asd]", 
+-- quantifiers "+" or "?", anchors "^" or "$" and the recursively defined alternative 
+-- "(cat|dog)" where "cat" could actually be any regular expression.
 
--- Can we kill this horrible prefix operator notation?
-regex :: Parser [Matchable]
-regex =
-  (++) <$> ((++) <$> (zeroOrOne startOfLine) <*> internal) <*> (zeroOrOne endOfLine)
+-- Extension: Add labels like this to the other parsers to give richer feedback.
+pLiteral :: Parser Pattern
+pLiteral = Literal <$> satisfy regularChar <?> "non special character."
 
--- What happens if there are unparsed characters? 
--- This probably means that the parsing is not implemented properly, but maybe maybe we need to 
--- check here and return Nothing if there is something left?
-parsePattern :: String -> Maybe [Matchable]
-parsePattern pattern = fst <$> runParser regex pattern
+pDigit :: Parser Pattern
+pDigit = Digit <$ chunk "\\d"
 
--- Partial Function: Does not match anchors for example.
-consume :: Matchable -> Char -> Bool
-consume (E (Literal l)) c = l == c
-consume (E Digit) c = isDigit c
-consume (E AlphaNumeric) c = isAlphaNum c
-consume (E wildcard) c = True
-consume (C (PosCharClass chars)) c = c `elem` chars
-consume (C (NegCharClass chars)) c = c `notElem` chars
+pAlphaNum :: Parser Pattern
+pAlphaNum = AlphaNum <$ chunk "\\w"
 
-consumeAll :: [Matchable] -> String -> Bool
-consumeAll [A EndOfLine] "" = True
-consumeAll [A EndOfLine] _ = False
-consumeAll [] _ = True
-consumeAll m "" = False
-consumeAll (m : (Q ZeroOrOne) : mx) (s : sx) =
-  (consume m s && consumeAll mx sx) || (consumeAll mx (s : sx))
-consumeAll (m : (Q OneOrMore) : mx) (s : sx) = 
-  consume m s && consumeAll mx (dropWhile (consume m) sx)
-consumeAll (e : ex) (s : sx) = consume e s && consumeAll ex sx
+pPosCharClass :: Parser Pattern
+pPosCharClass = do
+  void (char '[')
+  chars <- some (satisfy regularChar)
+  void (char ']')
+  return $ PosCharClass chars
 
+pNegCharClass :: Parser Pattern
+pNegCharClass = do
+  void (chunk "[^")
+  chars <- some (satisfy regularChar)
+  void (char ']')
+  return $ NegCharClass chars
 
+pStartAnchor :: Parser Pattern
+pStartAnchor = StartAnchor <$ char '^'
 
+pEndAnchor :: Parser Pattern
+pEndAnchor = EndAnchor <$ char '$'
 
+pZeroOrOne :: Parser Quantifier
+pZeroOrOne = ZeroOrOne <$ char '?'
 
+pOneOrMore :: Parser Quantifier
+pOneOrMore = OneOrMore <$ char '+'
 
+pQuantifier :: Parser Quantifier
+pQuantifier = pZeroOrOne <|> pOneOrMore
 
+pAlternative :: Parser Pattern
+pAlternative = do
+  void (char '(')
+  left <- pRegex
+  void (char '|')
+  right <- pRegex
+  void (char ')')
+  return $ Alternative left right
 
+pWildCard :: Parser Pattern
+pWildCard = Wildcard <$ char '.'
+-- More involved parsers.
+-- The full regex parser is built up in stages:
+--   + parse anything that a single character can match against;
+--   + allow a repetition quantifier (e.g. "+" or "?") to follow;
+--   + parse any number of these into a sequence, and then allow the whole sequence 
+--     to be preceeded by the start anchor ("^").
 
+pTerm :: Parser Pattern
+pTerm =
+  choice
+    [ pDigit,
+      pAlphaNum,
+      pNegCharClass,
+      pPosCharClass,
+      pAlternative,
+      pLiteral,
+      pEndAnchor,
+      pWildCard
+    -- TODO: Do we need pWildcard here?
+    ]
+
+-- Parse a Term as above, but pick up a repetition if it's given.
+pQuantifiedTerm :: Parser Pattern
+pQuantifiedTerm = applyQuantifier <$> pTerm <*> optional pQuantifier
+  where
+    applyQuantifier p Nothing = p
+    applyQuantifier p (Just q) = Repeat q p
+
+-- TODO:This needs a better name.
+-- helper function to unpack empty or singleton lists to the appropriate constructor.
+singletonOrSeq :: [Pattern] -> Pattern
+singletonOrSeq [] = Empty
+singletonOrSeq [pattern] = pattern
+singletonOrSeq patterns = Seq patterns
+
+pRegex :: Parser Pattern
+pRegex = singletonOrSeq <$> anchored
+  where
+    anchored = applyStartAnchor <$> optional pStartAnchor <*> many pQuantifiedTerm
+    -- If 'many' finds zero or one term, no need to wrap in a Comb.
+    applyStartAnchor Nothing patterns = patterns
+    applyStartAnchor (Just StartAnchor) patterns = StartAnchor : patterns
+
+-- Matching an Input
+
+-- All the base cases for now, trim the fat later.
+--   pattern   input     remaining input
+f :: Pattern -> String -> Maybe String
+f Empty s = Just s
+f Wildcard "" = Nothing
+f Wildcard (s:sx) = Just sx
+f EndAnchor s = if null s then Just "" else Nothing
+f (Literal _) "" = Nothing
+f (Literal c) (s:sx) = if c == s then Just sx else Nothing
+f Digit "" = Nothing
+f Digit (s:sx) = if isDigit s then Just sx else Nothing
+f AlphaNum "" = Nothing
+f AlphaNum (s:sx) = if isAlphaNum s then Just sx else Nothing
+f (PosCharClass cx) "" = Nothing
+f (PosCharClass cx) (s:sx) = if s `elem` cx then Just sx else Nothing
+f (NegCharClass cx) "" = Nothing
+f (NegCharClass cx) (s:sx) = if s `notElem` cx then Just sx else Nothing
+f (Repeat OneOrMore p) s = do
+  sx <- f p s
+  return $ dropWhile (isJust . f p . singleton) sx
+f (Repeat ZeroOrOne p) s = if isNothing match then Just s else match
+  where match = f p s
+f (Seq []) s = Just s
+f (Seq [p]) s = f p s
+f (Seq (p:px)) s = do
+  sx <- f p s
+  f (Seq px) sx
+f (Alternative left right) s = case f left s of
+  Just sx -> Just sx
+  Nothing -> f right s
+
+g :: String -> String -> Maybe String
+g pattern input = do
+  case parse pRegex "<stdin>" pattern of
+    Left bundle -> Nothing
+    Right (Seq (StartAnchor : patterns)) -> f (Seq patterns) input
+    Right result -> join $ find isJust (map (f result) (tails input))
 
 matchPattern :: String -> String -> Bool
-matchPattern pattern input = case parsePattern pattern of
-  Just (A StartOfLine : matchables) -> consumeAll matchables input 
-  Just elements -> any (consumeAll elements) (tails input)
-  Nothing -> error $ "Unhandled Pattern: " ++ pattern
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+matchPattern pattern input = isJust $ g pattern input
 
 main :: IO ()
 main = do
