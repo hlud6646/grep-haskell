b1fade946be024c26535f84829d6aa837aa7e7f0
Hugo Ludemann <hugo.ludemann@gmail.com>
2024-08-02 13:20:43 +1000

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,32 @@
 module Main where
 
-
-import Control.Monad (void, join)
+import Control.Monad (join, void)
 import Control.Monad.Combinators.Expr
 import Control.Monad.Identity (Identity, join)
 import Data.Char (isAlphaNum, isDigit)
-import Data.List (tails, singleton, find)
+import Data.List (find, singleton, tails, isPrefixOf)
+import Data.Maybe (isJust, isNothing, listToMaybe)
 import Data.Void
-import Data.Maybe (isJust, listToMaybe, isNothing)
 import System.Environment
 import System.Exit
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
--- gadt refactor?
 data Pattern
   = Empty
   | Literal Char
   | Digit
   | AlphaNum
+  | Wildcard
   | PosCharClass String
   | NegCharClass String
   | StartAnchor
   | EndAnchor
   | Repeat Quantifier Pattern
-  | Wildcard
   | Alternative Pattern Pattern
-  | Seq [Pattern] -- COMBination
+  | Capture Pattern
+  | BackRef Int
+  | Seq [Pattern]
   deriving (Eq, Show)
 
 -- Extension: Add Range Int Int constructor.
@@ -36,14 +36,15 @@ data Quantifier = ZeroOrOne | OneOrMore
 -- Parsec type parameters demystified: don't do anything fancy with erorrs, consume a String.
 type Parser = Parsec Void String
 
+-- Extension: Parse escaped characters.
+-- TODO: Should "\\" be in this list? I removed it to try and fix a weird behaviour in the tests.
 regularChar :: Char -> Bool
 regularChar c = c `notElem` "$^+?[]|()."
 
-
--- Basic parsers. 
+-- Basic parsers.
 -- These are the parsers for the basic elements of a regular expression, e.g. a literal
--- character like "a", the string ""\\d", character classes like "[asd]" or "[^asd]", 
--- quantifiers "+" or "?", anchors "^" or "$" and the recursively defined alternative 
+-- character like "a", the string ""\\d", character classes like "[asd]" or "[^asd]",
+-- quantifiers "+" or "?", anchors "^" or "$" and the recursively defined alternative
 -- "(cat|dog)" where "cat" could actually be any regular expression.
 
 -- Extension: Add labels like this to the other parsers to give richer feedback.
@@ -94,93 +95,196 @@ pAlternative = do
   void (char ')')
   return $ Alternative left right
 
+pCapture :: Parser Pattern
+pCapture = do
+  void (char '(')
+  pattern <- pRegex
+  void (char ')')
+  return $ Capture pattern
+
+
+
+-- Parsing something in barckets.
+-- pDev :: Parser (String , String)
+-- pDev = (,,,) <$> char '(' <*> pRegex <*> (optional ((char '|') *> pRegex)) <*> (char ')')
+pDev = do 
+  void $ char '('
+  (left, maybeRight) <-(,) <$> pRegex <*> (optional ((char '|') *> pRegex)) <* (char ')')
+  return $ case maybeRight of 
+    Nothing -> Capture left
+    Just right -> Capture (Alternative left right)
+
+pBackRef :: Parser Pattern 
+pBackRef = BackRef <$> (char '\\' *> integer)
+  where integer = read <$> some (satisfy isDigit)
+
 pWildCard :: Parser Pattern
 pWildCard = Wildcard <$ char '.'
+
 -- More involved parsers.
 -- The full regex parser is built up in stages:
 --   + parse anything that a single character can match against;
 --   + allow a repetition quantifier (e.g. "+" or "?") to follow;
---   + parse any number of these into a sequence, and then allow the whole sequence 
+--   + parse any number of these into a sequence, and then allow the whole sequence
 --     to be preceeded by the start anchor ("^").
 
-pTerm :: Parser Pattern
-pTerm =
+pUnquantified :: Parser Pattern
+pUnquantified =
   choice
     [ pDigit,
       pAlphaNum,
       pNegCharClass,
       pPosCharClass,
-      pAlternative,
-      pLiteral,
       pEndAnchor,
-      pWildCard
-    -- TODO: Do we need pWildcard here?
+      pDev,
+      pWildCard,
+      pBackRef,
+      pLiteral
     ]
 
--- Parse a Term as above, but pick up a repetition if it's given.
-pQuantifiedTerm :: Parser Pattern
-pQuantifiedTerm = applyQuantifier <$> pTerm <*> optional pQuantifier
+-- Parse an unquantified term as above, but pick up a postfix repetition character if given.
+pTerm :: Parser Pattern
+pTerm = applyQuantifier <$> pUnquantified <*> optional pQuantifier
   where
-    applyQuantifier p Nothing = p
-    applyQuantifier p (Just q) = Repeat q p
+    applyQuantifier pattern Nothing = pattern
+    applyQuantifier pattern (Just quantifier) = Repeat quantifier pattern
 
--- TODO:This needs a better name.
--- helper function to unpack empty or singleton lists to the appropriate constructor.
-singletonOrSeq :: [Pattern] -> Pattern
-singletonOrSeq [] = Empty
-singletonOrSeq [pattern] = pattern
-singletonOrSeq patterns = Seq patterns
+-- If a Seq has 0 or 1 element, unpack it.
+unpackSeq :: [Pattern] -> Pattern
+unpackSeq [] = Empty
+unpackSeq [pattern] = pattern
+unpackSeq patterns = Seq patterns
 
+absorbPrefix :: Maybe a -> [a] -> [a]
+absorbPrefix Nothing xs = xs
+absorbPrefix (Just x) xs = x : xs
+
+-- The final parser for a regular expression.
 pRegex :: Parser Pattern
-pRegex = singletonOrSeq <$> anchored
+pRegex = unpackSeq <$> anchored
   where
-    anchored = applyStartAnchor <$> optional pStartAnchor <*> many pQuantifiedTerm
-    -- If 'many' finds zero or one term, no need to wrap in a Comb.
-    applyStartAnchor Nothing patterns = patterns
-    applyStartAnchor (Just StartAnchor) patterns = StartAnchor : patterns
+    anchored = absorbPrefix <$> optional pStartAnchor <*> many pTerm
 
--- Matching an Input
-
--- All the base cases for now, trim the fat later.
---   pattern   input     remaining input
-f :: Pattern -> String -> Maybe String
-f Empty s = Just s
-f Wildcard "" = Nothing
-f Wildcard (s:sx) = Just sx
-f EndAnchor s = if null s then Just "" else Nothing
-f (Literal _) "" = Nothing
-f (Literal c) (s:sx) = if c == s then Just sx else Nothing
-f Digit "" = Nothing
-f Digit (s:sx) = if isDigit s then Just sx else Nothing
-f AlphaNum "" = Nothing
-f AlphaNum (s:sx) = if isAlphaNum s then Just sx else Nothing
-f (PosCharClass cx) "" = Nothing
-f (PosCharClass cx) (s:sx) = if s `elem` cx then Just sx else Nothing
-f (NegCharClass cx) "" = Nothing
-f (NegCharClass cx) (s:sx) = if s `notElem` cx then Just sx else Nothing
-f (Repeat OneOrMore p) s = do
-  sx <- f p s
-  return $ dropWhile (isJust . f p . singleton) sx
-f (Repeat ZeroOrOne p) s = if isNothing match then Just s else match
-  where match = f p s
-f (Seq []) s = Just s
-f (Seq [p]) s = f p s
-f (Seq (p:px)) s = do
-  sx <- f p s
-  f (Seq px) sx
-f (Alternative left right) s = case f left s of
+-- Matching an Input against a Pattern.
+consume :: Pattern -> String -> Maybe String
+consume Empty s = Just s
+consume Wildcard "" = Nothing
+consume Wildcard (s : sx) = Just sx
+consume EndAnchor s = if null s then Just "" else Nothing
+consume (Literal _) "" = Nothing
+consume (Literal c) (s : sx) = if c == s then Just sx else Nothing
+consume Digit "" = Nothing
+consume Digit (s : sx) = if isDigit s then Just sx else Nothing
+consume AlphaNum "" = Nothing
+consume AlphaNum (s : sx) = if isAlphaNum s then Just sx else Nothing
+consume (PosCharClass cx) "" = Nothing
+consume (PosCharClass cx) (s : sx) = if s `elem` cx then Just sx else Nothing
+consume (NegCharClass cx) "" = Nothing
+consume (NegCharClass cx) (s : sx) = if s `notElem` cx then Just sx else Nothing
+consume (Repeat OneOrMore p) s = do
+  sx <- consume p s
+  return $ dropWhile (isJust . consume p . singleton) sx
+consume (Repeat ZeroOrOne p) s = if isNothing match then Just s else match
+  where
+    match = consume p s
+consume (Seq []) s = Just s
+consume (Seq [p]) s = consume p s
+consume (Seq (p : px)) s = do
+  sx <- consume p s
+  consume (Seq px) sx
+consume (Alternative left right) s = case consume left s of
   Just sx -> Just sx
-  Nothing -> f right s
+  Nothing -> consume right s
 
-g :: String -> String -> Maybe String
-g pattern input = do
-  case parse pRegex "<stdin>" pattern of
-    Left bundle -> Nothing
-    Right (Seq (StartAnchor : patterns)) -> f (Seq patterns) input
-    Right result -> join $ find isJust (map (f result) (tails input))
+
+
+
+
+
+
+-- | Given a regular expression, some input and a list of captures, optionally
+-- return a tuple holding the matched part, the remainder of the string and 
+-- a list of captures.
+-- TODO: Are all base cases meaningful?
+f :: 
+  Pattern -> -- A parsed regular expression to match against.
+  String -> -- The input to compare the expression to.
+  [String] -> -- A list of the parts of the string matched by any previous captures (since the function is recursive)
+  Maybe (String, String, [String]) -- A tuple containing the consumed part of the string, the rest and the parts of the string matched by any previous captures.
+f Empty input captures = Just ("", input, captures)
+f Wildcard "" _ = Nothing
+f Wildcard (s : sx) captures = Just ([s], sx, captures)
+f EndAnchor s captures = if null s then Just ("", "", captures) else Nothing
+f (Literal _) "" _ = Nothing
+f (Literal c) (s : sx) captures = if c == s then Just ([s], sx, captures) else Nothing
+f Digit "" _ = Nothing
+f Digit (s : sx) captures = if isDigit s then Just ([s], sx, captures) else Nothing
+f AlphaNum "" _ = Nothing
+f AlphaNum (s : sx) captures = if isAlphaNum s then Just ([s], sx, captures) else Nothing
+f (PosCharClass _) "" _ = Nothing
+f (PosCharClass chars) (s : sx) captures = 
+  if s `elem` chars then Just ([s], sx, captures) else Nothing
+f (NegCharClass chars) "" _ = Nothing
+f (NegCharClass chars) (s : sx) captures = 
+  if s `notElem` chars then Just ([s], sx, captures) else Nothing
+-- This one is a bit horrible.
+-- TODO: Fix it. Currently, if the pattern matches exactly once the match will fail. This is wrong!
+
+
+
+
+f (Repeat OneOrMore pattern) input captures = do
+  -- If you have a pattern p, the repetitions of p are 
+  -- p, Seq [p, p], Seq [p, p, p] ...
+  let patternReps = (map Seq $ (map (\n -> take n (repeat pattern)) [1..]))
+  let bar = map (\p -> f p input captures) patternReps
+  (a, b, c) <- join . safeHead $ reverse (takeWhile isJust bar)
+  return $ (a, b, captures)
+
+
+
+
+
+ 
+f (Repeat ZeroOrOne pattern) input captures = 
+  if isNothing match then Just ("", input, captures) else match
+    where match = f pattern input captures
+f (Seq []) input captures = Just ("", input, captures)
+f (Seq [pattern]) input captures = f pattern input captures
+f (Seq (p : px)) input captures = do
+  (consumed, remainder, captures') <- f p input captures
+  (a, b, c) <- f (Seq px) remainder captures'
+  return $ (consumed ++ a, b, c)
+f (Alternative left right) input captures = case f left input captures of
+  Just x -> Just x
+  Nothing -> f right input captures
+
+
+f (Capture pattern) input captures = do 
+  (consumed, remaining, captures') <- f pattern input captures
+  return $ (consumed, remaining, captures' ++ [consumed])
+
+f (BackRef n) input captures = 
+  if capture `isPrefixOf` input then Just (capture, drop (length capture) input, captures) else Nothing
+  where capture = (captures !! (n - 1))
+
+
+
+
+
+
+
+
+  
+  
 
 matchPattern :: String -> String -> Bool
-matchPattern pattern input = isJust $ g pattern input
+matchPattern pattern input = isJust $ do
+  case parse pRegex "<stdin>" pattern of
+    Left bundle -> Nothing
+    Right (Seq (StartAnchor : patterns)) -> f (Seq patterns) input []
+    Right result -> join $ find isJust (map (\i -> f result i []) (tails input))
+
 
 main :: IO ()
 main = do
@@ -196,3 +300,9 @@ main = do
       if matchPattern pattern input_line
         then exitSuccess
         else exitFailure
+
+-- Backrefs :/
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead x = Just (head x)
+
